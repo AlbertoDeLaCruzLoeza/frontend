12ffe4aa537e336fa1ea410fd91dd5312681bc9e
AlbertoDeLaCruzLoeza <albertodelacruz684@gmail.com>
2025-06-02 17:00:44 -0600

    se mamo

--- src/api/authService.ts
+++ src/api/authService.ts
@@ -4,30 +4,26 @@ import axios from './axios';
 interface LoginData {
   email: string;
   password: string;
-  recaptchaToken: string;  
+  recaptchaToken: string;
 }
+
 interface RegisterData {
   email: string;
   password: string;
+  recaptchaToken?: string; // opcional si no se usa en registro
 }
 
+// POST: /auth/login
 export const login = (data: LoginData) => {
-<<<<<<< HEAD
   return axios.post<{ access_token: string }>('/auth/login', data, {
-    withCredentials: true, 
+    withCredentials: true,
     headers: {
       'Content-Type': 'application/json',
     },
   });
 };
 
-
 // POST: /auth/register
-=======
-  return axios.post<{ token: string }>('/auth/login', data);
-};
-
->>>>>>> d6a2411bcf253420affc705514ac1f9205218a9c
 export const register = (data: RegisterData) => {
   return axios.post('/auth/register', data);
 };
