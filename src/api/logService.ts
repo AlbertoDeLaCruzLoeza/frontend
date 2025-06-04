6d3ebd2ef0e48072882485ff6200367931435e0c
AlbertoDeLaCruzLoeza <albertodelacruz684@gmail.com>
2025-06-04 15:19:54 -0600

    no la cague

--- src/api/logService.ts
+++ src/api/logService.ts
@@ -5,15 +5,6 @@ const API_URL = 'https://back-front-y42m.onrender.com';
 
 export const getLogs = async (params?: any) => {
   const token = localStorage.getItem('token');
-<<<<<<< HEAD
-  return axios.get(${API_URL}/logs, {
-    headers: {
-      Authorization: Bearer ${token},
-    },
-    params,
-  });
-};
-=======
   return axios.get(`${API_URL}/logs`, {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -21,4 +12,3 @@ export const getLogs = async (params?: any) => {
     params,
   });
 };
->>>>>>> c146afc192d1768b89ebae01b628249dacd4ef8e
