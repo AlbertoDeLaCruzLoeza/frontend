c146afc192d1768b89ebae01b628249dacd4ef8e
Wendy Novelo <wendynovelo764@gmail.com>
2025-06-04 03:28:55 -0600

    logs ya jala

--- src/api/logService.ts
+++ src/api/logService.ts
@@ -1,8 +1,14 @@
 // src/api/logService.ts
 import axios from 'axios';
 
-const API_URL = 'https://back-alberto.onrender.com';
+const API_URL = 'https://back-front-y42m.onrender.com';
 
-export const getLogs = async () => {
-  return axios.get(`${API_URL}/logs`);
+export const getLogs = async (params?: any) => {
+  const token = localStorage.getItem('token');
+  return axios.get(`${API_URL}/logs`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    params,
+  });
 };
